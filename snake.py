0c5ccefa61d15d07455dcc3b434b6adb6f3aab95
Shivam <shivamkonkar25@gmail.com>
2025-09-24 22:28:13 +0530

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((750,750))
+
+pygame.display.set_caption("Retro Snake")
+
+clock = pygame.time.Clock()
+
+while True:a
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
