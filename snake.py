5018b708856471f1959d84e8f46abd19e73d1291
Shivam <shivamkonkar25@gmail.com>
2025-09-24 22:30:48 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -3,17 +3,21 @@ import sys
 
 pygame.init()
 
+GREEN = (173,204,96)
+DARK_GREEN = (43,51,24)
+
 screen = pygame.display.set_mode((750,750))
 
 pygame.display.set_caption("Retro Snake")
 
 clock = pygame.time.Clock()
 
-while True:a
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    screen.fill(GREEN)
     pygame.display.update()
     clock.tick(60)
